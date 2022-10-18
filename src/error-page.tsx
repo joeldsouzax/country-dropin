5d946aefc161db51ff471cc1b5f8022eae375e6b
Joel DSouza <joeldsouzax@gmail.com>
2022-10-18 21:28:54 +0200

    feat(country-dropin): added error-page on global level

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -6,7 +6,13 @@ import { RouteError } from '@/types';
 
 const ErrorPage: React.FC = () => {
   const error = useRouteError() as RouteError;
-  return <div></div>;
+  return (
+    <div id="country-dropin-error-page">
+      <h1>Welp!</h1>
+      <h2>Something went wrong</h2>
+      <h4>{error.message || error.statusText}</h4>
+    </div>
+  );
 };
 
 export default ErrorPage;
