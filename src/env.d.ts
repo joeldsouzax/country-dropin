f792199da156159c626ff1c62eb3ce18e2e9cf62
Joel DSouza <joeldsouzax@gmail.com>
2022-10-18 20:48:10 +0200

    build(country-dropin): added env variable types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,14 @@
 /** @format */
 
 /// <reference types="vite/client" />
+
+/**
+ * specify environment variable types
+ */
+interface ImportMetaEnv {
+  readonly VITE_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
