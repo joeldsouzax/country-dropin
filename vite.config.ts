fdb42182814b2737738e76c20b29399751bb7b1a
Joel DSouza <joeldsouzax@gmail.com>
2022-10-18 20:45:13 +0200

    build(country-dropin): added vitest type reference in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 /** @format */
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  test: {},
 });
