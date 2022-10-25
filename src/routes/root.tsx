b3a20046b598739951398ae872a34c39aae8b394
Joel DSouza <joeldsouzax@gmail.com>
2022-10-25 09:12:54 +0200

    build(country-dropin): removed tailwind and added mui

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,22 +2,13 @@
 
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 const Root: React.FC = () => {
   return (
-    <div className="w-full p-4">
-      <main
-        role="main"
-        className="w-full flex flex-col content-center justify-center"
-      >
-        <div className="w-full sm:w-1/2 lg:w-1/3 bg-gray-50 rounded-xl m-auto">
-          <div className="bg-white rounded shadow px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <h1>Hello</h1>
-          </div>
-          <Outlet />
-        </div>
-      </main>
-    </div>
+    <Container>
+      <Outlet />
+    </Container>
   );
 };
 
--- src/services/country.ts
+++ src/services/country.ts
@@ -6,10 +6,11 @@ import { api } from './api';
 
 const countryApi = api.injectEndpoints({
   endpoints: (build) => ({
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
     getCountries: build.query<GetCountriesResponse, void>({
       query: () => countries,
       providesTags: (result) => {
-        return result
+        return result != null
           ? [
               ...result.map(({ id }) => ({ type: 'Countries', id } as const)),
               { type: 'Countries', id: 'LIST' },
